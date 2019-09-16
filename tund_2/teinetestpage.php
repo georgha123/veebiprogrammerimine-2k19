d6c5bb9dbad373941307978d4b84739cf8eeffb1
Georg-Henri Allas <georgha123@users.noreply.github.com>
2019-09-16 19:54:55 +0300

    kodutöö

--- tund_2/teinetestpage.php
+++ tund_2/teinetestpage.php
@@ -1,5 +1,18 @@
 <?php
   $userName = "Georg";
+  $fullTimeNow = date("d.m.Y H:i:s");
+  $hourNow = date("H") ;
+  $partOfDay = "öö";
+  
+  if($hourNow >= 5){
+	  $partOfDay = "hommik";
+  }
+  elseif($hourNow >= 11) {
+	  $partOfDay = "lõuna";
+  }
+  else ($hourNow >=16) {
+	  $partOfDay = "õhtu";
+  }
 ?>
 
 <!DOCTYPE html>
@@ -18,11 +31,12 @@
 <?php
 echo "<h1>" .$userName .", veebiprogrammeerimine</h1>";
 
-?>
-
+  ?>
   <p>See veebilieht on loodud õppetöö käigus ning ei sisalda mingit tõsiseltvõetavat sisu</p>
-
-
+<hr>
+ <?php
+  echo "<P> Lehe avamise hetkel oli aeg: " .$fullTimeNow .".</p>" .$partOfDay
+ ?>
 </body>
 
 </html>
